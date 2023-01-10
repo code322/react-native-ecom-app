1cf3087af6463675c90554ab3dca62e3a5753719
Tcodingg <princetlive@yahoo.com>
2023-01-10 18:01:23 -0500

    create products route

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const productsRoute = require('./routes/productsRoute');
 
 const PORT = 5000 || process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('main');
 });
+
+app.use('/api/products', productsRoute);
 
 app.listen(PORT, () => console.log(`App is running in port ${PORT}`));
