f56cfce29bc2752758c61e2f11f8b373aa0cf449
Tcodingg <princetlive@yahoo.com>
2023-01-10 19:09:22 -0500

    creat get product by id route

--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const getAllProducts = require('../controllers/productsController');
+const {
+  getAllProducts,
+  getProduct,
+} = require('../controllers/productsController');
 const router = express.Router();
 
 router.get('/', getAllProducts);
+router.get('/:id', getProduct);
 
 module.exports = router;
