7c5cf9ee3f25f82bfa38bb3468a931528a772913
Tcodingg <princetlive@yahoo.com>
2023-01-13 13:57:05 -0500

    change the background of category when selected

--- frontend/components/Categories/Categories.js
+++ frontend/components/Categories/Categories.js
@@ -1,15 +1,28 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { style } from './Style';
 
-const Categories = ({ title, Icon }) => {
+const Categories = ({ title, Icon, id, setSelectedItem, selectedItem }) => {
   return (
-    <View style={[style.container]}>
-      <View style={[style.iconContainer, { backgroundColor: '#444' }]}>
-        <Icon width={40} height={40} fill={'#909090'} />
+    <TouchableOpacity
+      activeOpacity={0.8}
+      style={style.container}
+      onPress={() => setSelectedItem(id)}
+    >
+      <View
+        style={[
+          style.iconContainer,
+          { backgroundColor: selectedItem === id ? '#000' : '#fff' },
+        ]}
+      >
+        <Icon
+          width={40}
+          height={40}
+          fill={selectedItem === id ? '#fff' : '#909090'}
+        />
       </View>
       <Text style={style.title}>{title}</Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
