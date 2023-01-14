0405264072545190ba34e16b41742a6d2a06e5a1
Tcodingg <princetlive@yahoo.com>
2023-01-14 18:16:07 -0500

    rename the selectedItem to selectedCategory. Create a product screen.

--- frontend/components/Categories/Categories.js
+++ frontend/components/Categories/Categories.js
@@ -3,26 +3,26 @@ import React from 'react';
 import { style } from './Style';
 
 const Categories = ({ ...props }) => {
-  const { title, Icon, setSelectedItem, selectedItem } = props;
+  const { title, Icon, setSelectedCategory, selectedCategory } = props;
 
   return (
     <TouchableOpacity
       activeOpacity={0.8}
       style={style.container}
-      onPress={() => setSelectedItem(title)}
+      onPress={() => setSelectedCategory(title)}
     >
       <View
         style={[
           style.iconContainer,
           {
-            backgroundColor: selectedItem === title ? '#000' : '#F5F5F5',
+            backgroundColor: selectedCategory === title ? '#000' : '#F5F5F5',
           },
         ]}
       >
         <Icon
           width={40}
           height={40}
-          fill={selectedItem === title ? '#F5F5F5' : '#909090'}
+          fill={selectedCategory === title ? '#F5F5F5' : '#909090'}
         />
       </View>
       <Text style={style.title}>{title}</Text>
