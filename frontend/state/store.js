aefbc62824047b855dd04c1e7a13ba4b47e3f158
Tcodingg <princetlive@yahoo.com>
2023-01-15 15:24:31 -0500

    install redux persist and AsyncStorage dependency

--- frontend/state/store.js
+++ frontend/state/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import productsSlice from './productsSlice';
 import productSlice from './productSlice';
 import cartSlice from './cartSlice';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const store = configureStore({
   reducer: {
