4983f2faed6a568df89e4a99218cbcc1c8b87ed7
Tcodingg <princetlive@yahoo.com>
2023-01-15 15:52:54 -0500

    install redux, redux-persist and redux-thunk dependecies. Configer the store with redux-persistor

--- frontend/App.js
+++ frontend/App.js
@@ -5,19 +5,24 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Tabs } from './navigation/Tabs';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 export default function App() {
+  let persistor = persistStore(store);
   const Stack = createStackNavigator();
 
   return (
     <NavigationContainer>
       <Provider store={store}>
-        <SafeAreaProvider>
-          <StatusBar style='auto' />
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name='Tabs' component={Tabs} />
-          </Stack.Navigator>
-        </SafeAreaProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <SafeAreaProvider>
+            <StatusBar style='auto' />
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+              <Stack.Screen name='Tabs' component={Tabs} />
+            </Stack.Navigator>
+          </SafeAreaProvider>
+        </PersistGate>
       </Provider>
     </NavigationContainer>
   );
--- frontend/state/store.js
+++ frontend/state/store.js
@@ -1,15 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
 import productsSlice from './productsSlice';
 import productSlice from './productSlice';
 import cartSlice from './cartSlice';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+const reducers = combineReducers({
+  productsSlice,
+  productSlice,
+  cartSlice,
+});
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-  reducer: {
-    productsSlice,
-    productSlice,
-    cartSlice,
-  },
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
 
 export default store;
