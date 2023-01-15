9afbdb0b754b5a7581039a9c918546e777ca591f
Tcodingg <princetlive@yahoo.com>
2023-01-15 14:58:02 -0500

    add cartslice to the store

--- frontend/state/store.js
+++ frontend/state/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsSlice from './productsSlice';
 import productSlice from './productSlice';
+import cartSlice from './cartSlice';
 
 const store = configureStore({
   reducer: {
     productsSlice,
     productSlice,
+    cartSlice,
   },
 });
 
