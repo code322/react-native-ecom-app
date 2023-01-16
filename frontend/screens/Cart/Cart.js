1429859677c9e049b288111356762124aaa6b307
Tcodingg <princetlive@yahoo.com>
2023-01-16 11:48:58 -0500

    show empty cart icon and shop now button if the cart is empty

--- frontend/screens/Cart/Cart.js
+++ frontend/screens/Cart/Cart.js
@@ -1,11 +1,9 @@
-import { View, Text, Image, FlatList, Platform } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, Platform } from 'react-native';
 import React from 'react';
 import { style } from './Style';
 import { useSelector } from 'react-redux';
 import Container from '../../components/Container/Container';
 import { Ionicons } from '@expo/vector-icons';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { colors } from '../../theme/colors';
 import CartItem from '../../components/CartItem/CartItem';
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
@@ -28,6 +26,23 @@ const Cart = ({ navigation }) => {
     bottomTab = useBottomTabBarHeight();
   } else {
     bottomTab = insets.bottom;
+  }
+  if (cart.length < 1) {
+    return (
+      <View style={style.emptyCartContainer}>
+        <Ionicons name='ios-cart-outline' size={104} color='black' />
+        <Text style={style.emptyCartTitle}>Your cart is empty</Text>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={style.shopNow}
+          onPress={() =>
+            navigation.navigate('ProductsTab', { screen: 'Products' })
+          }
+        >
+          <Text style={style.shopNowTitle}>Shop Now!</Text>
+        </TouchableOpacity>
+      </View>
+    );
   }
 
   return (
--- frontend/screens/Cart/Style.js
+++ frontend/screens/Cart/Style.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import { colors } from '../../theme/colors';
 
 export const style = StyleSheet.create({
   header: {
@@ -8,5 +9,24 @@ export const style = StyleSheet.create({
   },
   container: {
     marginTop: 20,
+  },
+  emptyCartContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  emptyCartTitle: {
+    // fontWeight: 'bold',
+    marginTop: -10,
+  },
+  shopNow: {
+    backgroundColor: colors.black,
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+    borderRadius: 10,
+    marginTop: 40,
+  },
+  shopNowTitle: {
+    color: colors.white,
   },
 });
