94de91182c6e6dc2f0234df9be6f9731709514e2
Tcodingg <princetlive@yahoo.com>
2023-01-17 10:59:17 -0500

    add signin header and back navigation

--- frontend/screens/SignIn/SignIn.js
+++ frontend/screens/SignIn/SignIn.js
@@ -1,11 +1,24 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import Container from '../../components/Container/Container';
+import { Ionicons } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { colors } from '../../theme/colors';
+import { styles } from './Style';
 
-const SignIn = () => {
+const SignIn = ({ navigation }) => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <Container>
+      <View style={styles.navContainer}>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={() => navigation.goBack()}
+        >
+          <Ionicons name='arrow-back-outline' size={40} color={colors.black} />
+        </TouchableOpacity>
+        <Text style={styles.navTitle}>Sign In</Text>
+      </View>
+    </Container>
   );
 };
 
---
+++ frontend/screens/SignIn/Style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  navContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  navTitle: {
+    fontSize: 20,
+    marginLeft: 10,
+    textTransform: 'capitalize',
+  },
+});
