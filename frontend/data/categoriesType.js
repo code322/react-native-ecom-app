99231a22acd7c1f8d08fbaa3f9eb5fab510fd4af
Tcodingg <princetlive@yahoo.com>
2023-01-20 10:52:23 -0500

    change the categories images

--- frontend/data/categoriesType.js
+++ frontend/data/categoriesType.js
@@ -1,9 +1,14 @@
-import Icon from '../assets/images/chair.svg';
+import all from '../assets/images/star.svg';
+import chair from '../assets/images/chair.svg';
+import table from '../assets/images/table.svg';
+import armchair from '../assets/images/sofa 1.svg';
+import bed from '../assets/images/bed 1.svg';
+import lamp from '../assets/images/lamp.svg';
 export const categoriesType = [
-  { id: 1, title: 'all', image: Icon },
-  { id: 2, title: 'chair', image: Icon },
-  { id: 3, title: 'table', image: Icon },
-  { id: 4, title: 'armchair', image: Icon },
-  { id: 5, title: 'bed', image: Icon },
-  { id: 6, title: 'lamp', image: Icon },
+  { id: 1, title: 'all', image: all },
+  { id: 2, title: 'chair', image: chair },
+  { id: 3, title: 'table', image: table },
+  { id: 4, title: 'armchair', image: armchair },
+  { id: 5, title: 'bed', image: bed },
+  { id: 6, title: 'lamp', image: lamp },
 ];
