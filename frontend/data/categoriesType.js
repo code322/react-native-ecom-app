b6cd18218be163748329b0a9ea6e6883844db766
Tcodingg <princetlive@yahoo.com>
2023-01-19 23:43:47 -0500

    fix a typo and change the popular category to all

--- frontend/data/categoriesType.js
+++ frontend/data/categoriesType.js
@@ -1,9 +1,9 @@
 import Icon from '../assets/images/chair.svg';
 export const categoriesType = [
-  { id: 1, title: 'popular', image: Icon },
+  { id: 1, title: 'all', image: Icon },
   { id: 2, title: 'chair', image: Icon },
   { id: 3, title: 'table', image: Icon },
   { id: 4, title: 'armchair', image: Icon },
   { id: 5, title: 'bed', image: Icon },
-  { id: 6, title: 'lamb', image: Icon },
+  { id: 6, title: 'lamp', image: Icon },
 ];
